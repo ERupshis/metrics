0458cc7a03f8a63ab8280a4c8cdd489f1c23faae
Edgar Rupshis <edgar.rupshis@glera-games.com>
2023-09-03 21:03:16 +0300

    added json metrics batches.

--- internal/server/controllers/base.go
+++ internal/server/controllers/base.go
@@ -138,13 +138,13 @@ func (c *BaseController) jsonPostBatchHandler(w http.ResponseWriter, metrics []n
 		c.addMetricFromMessage(&metric)
 	}
 
-	body, err := json.Marshal(metrics)
+	_, err := json.Marshal(metrics)
 	if err != nil {
 		c.logger.Info("[BaseController::jsonPostBatchHandler] Error occurs during batch marshaling for response: %v", err)
 	}
 
 	w.Header().Add("Content-Type", "application/json")
-	_, _ = w.Write(body)
+	_, _ = w.Write([]byte("{}"))
 }
 
 func (c *BaseController) jsonPostHandler(w http.ResponseWriter, data *networkmsg.Metric) {
