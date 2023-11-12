2bcced768ed435dc7d02e0c32019c214a66ecf58
Edgar Rupshis <73851167+ERupshis@users.noreply.github.com>
2023-11-12 17:38:11 +0200

    added comments for retryer

--- internal/retryer/retryer.go
+++ internal/retryer/retryer.go
@@ -1,3 +1,4 @@
+// Package retryer provides functions to recall some action with defined interval in case of error.
 package retryer
 
 import (
@@ -7,7 +8,22 @@ import (
 	"github.com/erupshis/metrics/internal/logger"
 )
 
+// defIntervals default intervals to retry action.
 var defIntervals = []int{1, 3, 5}
+
+// RetryCallWithTimeout retries the execution of the specified callback function
+// considering time intervals until successful completion or context expiration.
+// The function takes a context, a base logger, an array of waiting intervals (in seconds),
+// an array of repeatable errors, and a callback function to be retried. In case of an error
+// occurring during the callback execution, the function will be retried within the specified intervals.
+// Parameters:
+//   - ctx: Execution context providing cancellation and timeout.
+//   - log: Base logger for logging information about retry attempts.
+//   - intervals: Array of time intervals (in seconds) between retry attempts.
+//     If intervals is nil, default values will be used.
+//   - repeatableErrors: Array of errors for which retry attempts are considered valid.
+//   - callback: Callback function to be retried in case of an error.
+// Returns an error that occurred in the last attempt or nil in case of successful execution.
 
 func RetryCallWithTimeout(ctx context.Context, log logger.BaseLogger, intervals []int, repeatableErrors []error, callback func(context.Context) error) error {
 	var err error
@@ -42,6 +58,14 @@ func RetryCallWithTimeout(ctx context.Context, log logger.BaseLogger, intervals 
 	return err
 }
 
+// canRetryCall checks whether the given error is considered retryable
+// based on the provided array of repeatable errors.
+// If repeatableErrors is nil, the function considers all errors as retryable.
+// Parameters:
+//   - err: The error to be checked for retryability.
+//   - repeatableErrors: Array of errors that are considered retryable.
+//
+// Returns true if the error is retryable, otherwise returns false.
 func canRetryCall(err error, repeatableErrors []error) bool {
 	if repeatableErrors == nil {
 		return true
